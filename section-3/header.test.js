122e51abf09bdb8f17ec3bcccb89911f196a58d6
ahazimeh <hazimeh95@gmail.com>
2022-10-25 10:57:10 +0300

    generating session signature

--- section-3/header.test.js
+++ section-3/header.test.js
@@ -27,3 +27,22 @@ test('clicking login starts oauth flow', async () => {
 
     expect(url).toMatch(/accounts\.google\.com/);
 })
+
+test('When signed in, show logout button', async () => {
+    const id = '635539939711130c839d6dc6';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    console.log(sessionString, sig)
+});
